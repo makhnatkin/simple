3b236240563157ecc1b2bb6fada0e3dd78322d51
Sergey Makhnatkin <sergey.makhnatkin@gmail.com>
2022-10-19 23:02:33 +0300

    Fixed some to _

--- src/examples/05__CSSModules/Some.jsx
+++ src/examples/05__CSSModules/Some.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import styles from 'Some.module.css'; // CSS modules
 
-const Some = ({ title }) => <div className={`${styles.some} ${styles.danger}`}>
+const Some = ({ title }) => <div className={`${styles._} ${styles.danger}`}>
     <div className={styles.title}>{title}</div>
 </div>;
 
